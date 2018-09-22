d050110c653ef3f505d3e72b4fec34c38819e025
ASchneidman <alexmarkschneidman@gmail.com>
2018-09-21 23:09:10 -0400

    download update

--- HackCMU2018/crawler/download_all.py
+++ HackCMU2018/crawler/download_all.py
@@ -1,14 +1,20 @@
 from github import Github
 import wget
 import os, requests
+from datetime import datetime
 def download():
     cwd = os.getcwd()
-    g = Github("hackcmucrawler",'''%uAbPPe^@7U9''')
+    g = Github("hackcmucrawler",'''%uAbPPe^@7U9''',per_page=100)
     sum_lines = 0
-    num_files = 0
+    num_files = 778
     repos = g.get_repos()
     for repo in repos:
+        print(g.rate_limiting)
         try:
+            if (not (repo.language == "C" or repo.language == "C++" or repo.language == "Cpp")):
+                print("not C")
+                continue
+            print("C!")
             branches_objs = repo.get_branches()
             branches = [b.name for b in branches_objs]
             if "master" in branches:
@@ -20,14 +26,18 @@ def download():
                 if (len(cpp_files) > 0):
                     #contents = [repo.get_file_contents(c) for c in cpp_files]
                     for file in cpp_files:
+                        file_name = str(file.path).replace("/","")
                         url = "https://raw.githubusercontent.com/" + str(repo.owner.login) + "/" + str(repo.name) + "/master/" + str(file.path)
-                        output_path = os.path.join(cwd,os.path.join("cpp_files",str(num_files) + ".cpp"))
+                        output_path = os.path.join(cwd,os.path.join("cpp_files",file_name))
+                        #If already downloaded, dont download
+                        if (os.path.exists(output_path)):
+                            print("Already downloaded")
+                            continue
                         print(url)
                         r = requests.get(url,stream=True)
                         file_length = len([l for l in r.iter_lines()])
                         if (file_length < 50 or file_length > 2500):
                             break
-                        print(url)
                         wget.download(url, out=output_path)
                         sum_lines += file_length
                         num_files += 1
@@ -40,3 +50,8 @@ def download():
 
 if __name__ == "__main__":
     download()
+
+def test():
+    g = Github("hackcmucrawler",'''%uAbPPe^@7U9''')
+    print(g.rate_limiting)
+    print(datetime.fromtimestamp(g.rate_limiting_resettime))
