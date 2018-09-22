bf0c6e22e507e0934ff6a2e006a2f005b3c7d23c
ASchneidman <alexmarkschneidman@gmail.com>
2018-09-22 13:00:21 -0400

    added results page

--- HackCMU2018/StyleIO/StyleIOApp/urls.py
+++ HackCMU2018/StyleIO/StyleIOApp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('results/<int:num_errors>',views.results,name='results')
 ]
--- HackCMU2018/StyleIO/StyleIOApp/views.py
+++ HackCMU2018/StyleIO/StyleIOApp/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UploadFileForm
 import os
 
@@ -9,14 +9,22 @@ def handle_uploaded_file(f):
     with open(dir, 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
+        return 10
+
 
 def index(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         print(request.FILES)
         if form.is_valid():
-            handle_uploaded_file(request.FILES['file'])
-            return HttpResponse("Got it!")
+            num_errors = handle_uploaded_file(request.FILES['file'])
+            return redirect('results',num_errors)
     else:
         form = UploadFileForm()
     return render(request, 'index.html', {'form': form})
+
+def results(request,num_errors):
+    context = {
+        'num_errors': num_errors
+    }
+    return render(request,'results.html',context)
